79c2414598a1a734c8f0ac11de2d7b97fb36be0c
user <user@DESKTOP-7P1PL3O>
2022-03-31 10:37:14 +0100

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Arrays
+{
+    public class Programs
+    {
+        static void Main()
+        {
+            int[] numbers = new int[4] { 9, 3, 88, 45 };
+            bool[] boolArray = new bool[3] { true, false, true };
+            numbers[2] = 200;
+            numbers[3] = numbers[0];
+            numbers[2] = numbers[2] + 1;
+            Console.WriteLine(numbers[3]);
+            Console.WriteLine(numbers[2]);
+        }
+    }
+}
+    //Console.WriteLine("Hello, World!");
